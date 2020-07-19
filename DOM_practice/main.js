6749164f3ee7d70be526b178343d9d21c2fca4f8
sanada <gigglingirl1225@gmail.com>
2020-07-19 21:09:47 +0900

    DOMの追加の練習

--- DOM_practice/main.js
+++ DOM_practice/main.js
@@ -16,11 +16,18 @@
   // }
 
   document.querySelector('button').addEventListener('click', () => {
-    const item2 = document.createElement('li');
-    item2.textContent = 'item 2';
+    // const item2 = document.createElement('li');
+    // item2.textContent = 'item 2';
 
-    const ulNode = document.querySelector('ul');
-    ulNode.appendChild(item2);
+    // const ulNode = document.querySelector('ul');
+    // ulNode.appendChild(item2);
+
+    const item0 = document.querySelectorAll('li')[0];
+    const copy = item0.cloneNode(true);
+
+    const ul = document.querySelector('ul');
+    const item2 = document.querySelectorAll('li')[2];
+    ul.insertBefore(copy, item2);
 
   })
 }
